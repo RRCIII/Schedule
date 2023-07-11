1e197823eac8aca41685874d45ba6bc7e158d3b9
Ricky Carter <randolphcarter5@gmail.com>
2023-07-11 16:25:10 -0400

    created variable to format dayjs to display in hours.

--- script.js
+++ script.js
@@ -1,58 +1,93 @@
 //jQuery shorthand to ensure code is executed after the browser has finished rendering all elements in HTML
+
 $(function () {
-  //created current 24-hr clock time via dayjs()
-  const hr = dayjs().format('HH');
- 
-  //color coding time block to each element 
-  const colorCode = () => {
-    $('.time-block').each(function() {
-      const timeBlock = this.id; // 'this' keyword directs to the current element being iterated 
-      
-      //conditional statement rendering color coding to represent present, future, and past depending on the current hr 
-      if (timeBlock === hr) {
-        $(this).removeClass('past future').addClass('present');
-      } else if (timeBlock > hr) {
-        $(this).removeClass('past present').addClass('future');
-      } else {
-        $(this).removeClass('present future').addClass('past');
-      }
-    })
-  }
+ const hr = dayjs().format('HH');
 
-  //display the current date/time to header of HTML
-  const dtHeader = () => {
-    const currentDay = $('#currentDay');
-    const date = dayjs().format('ddd. MMMM D, YYYY');
-    const time = dayjs().format('hh:mm:ss a');
 
-    currentDay.text(`${date} | ${time}`);
-   
-  }
 
-  // creating an eventListenter to 'save' local storage fxn, once 'save' is clicked 
-  const saveBtn = () => {
-    $('.btn').on('click', function () {
-      const key = $(this).parent().attr('id');
-      const value = $(this).siblings('textarea').val(); // input from user
 
-      localStorage.setItem(key, value);
-    })
-  }
 
-  //user abe to render value when page is refreshed
-  const grabEvent = () => {
-    $('.time-block').each(function() {
-      const key = $(this).attr('id');
-      const value = localStorage.getItem(key);
 
-      $(this).children('textarea').val(value);
-    })
-  }
 
-  // call fxns here and interval set to update every second
-  setInterval(colorCode, 1000);
-  setInterval(dtHeader, 1000);
-  saveBtn();
-  grabEvent();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
+
+
+
+
+
+
+  //created current 24-hr clock time via dayjs()
+  // const hr = dayjs().format('HH');
+ 
+  //color coding time block to each element 
+  // const colorCode = () => {
+    // $('.time-block').each(function() {
+      // const timeBlock = this.id; // 'this' keyword directs to the current element being iterated 
+      
+      //conditional statement rendering color coding to represent present, future, and past depending on the current hr 
+      // if (timeBlock === hr) {
+        // $(this).removeClass('past future').addClass('present');
+      // } else if (timeBlock > hr) {
+        // $(this).removeClass('past present').addClass('future');
+      // } else {
+        // $(this).removeClass('present future').addClass('past');
+      // }
+    // })
+  // }
+
+  //display the current date/time to header of HTML
+  // const dtHeader = () => {
+    // const currentDay = $('#currentDay');
+    // const date = dayjs().format('ddd. MMMM D, YYYY');
+    // const time = dayjs().format('hh:mm:ss a');
+
+    // currentDay.text(`${date} | ${time}`);
+   
+  // }
+
+  // creating an eventListenter to 'save' local storage fxn, once 'save' is clicked 
+  // const saveBtn = () => {
+    // $('.btn').on('click', function () {
+      // const key = $(this).parent().attr('id');
+      // const value = $(this).siblings('textarea').val(); // input from user
+
+      // localStorage.setItem(key, value);
+    // })
+  // }
+
+  //user abe to render value when page is refreshed
+  // const grabEvent = () => {
+    // $('.time-block').each(function() {
+      // const key = $(this).attr('id');
+      // const value = localStorage.getItem(key);
+
+      // $(this).children('textarea').val(value);
+    // })
+  // }
+
+  // call fxns here and interval set to update every second
+  // setInterval(colorCode, 1000);
+  // setInterval(dtHeader, 1000);
+  // saveBtn();
+  // grabEvent();
+
+
